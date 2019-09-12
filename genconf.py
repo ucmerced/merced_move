36b078de2ad1215a99f39768cd29704d6c3b1c92
Matthias Bussonnier <mbussonnier@ucmerced.edu>
2019-09-12 12:19:42 -0700

    test

---
+++ genconf.py
@@ -0,0 +1,12 @@
+
+import jinja2
+
+e = jinja2.Environment()
+
+tpl = e.from_string(open('sa,ple.tpl').read())
+
+hosts = ['mrcd%02d'%i for i in range(1,89)] + ['mrcdg%02d'%i for i in range(1,7)]
+
+for hostname in hosts:
+    with open(f'py/py-{hostname}.sub', 'w') as f:
+        f.write(tpl.render(hostname=hostname))
